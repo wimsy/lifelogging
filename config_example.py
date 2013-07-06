2bfe9145a8a0fe3f397decebb7d7262cec358115
Michael Wimsatt <mike.wimsatt@gmail.com>
2013-07-06 14:14:36 -0400

    Added example config.py file

---
+++ config_example.py
@@ -0,0 +1,22 @@
+# User-specific keys and ids
+
+from datetime import date
+
+# Flask app setup
+SECRET_KEY = 'SAMPLE'
+
+# Pushover API setup for iPhone/Android notifications
+PUSHOVER_APP_TOKEN = 'SAMPLE'
+PUSHOVER_USER_KEY = 'SAMPLE'
+
+# Moves API setup
+MOVES_CLIENT_ID = 'SAMPLE'
+MOVES_CLIENT_SECRET = 'SAMPLE'
+MOVES_ACCESS_TOKEN = 'SAMPLE'
+MOVES_FIRST_DATE = date(SAMPLE,SAMPLE,SAMPLE)
+MOVES_BACKLOG_WINDOW = 6  # Number of complete prior weeks
+
+# OpenPaths API setup
+OP_ACCESS = "SAMPLE"
+OP_SECRET = "SAMPLE"
+
